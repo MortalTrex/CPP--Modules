29f967fa1bea4b9596bfc751ca3fbd2ebc0f06eb
Ralph Balazs <rbalazs@f1r10s5.clusters.42paris.fr>
2025-06-17 20:01:47 +0200

    strtod

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -23,14 +23,16 @@ ScalarConverter::~ScalarConverter()
 
 void ScalarConverter::convert(const std::string &literal)
 {
+    char *ptr;
+    double number = strtod(literal.c_str(), &ptr);
     std::cout << "Converting literal: " << literal << std::endl;
 
     std::cout << "char: ";
     std::cout << std::endl;
-    std::cout << "int : ";
+    std::cout << "int : " << atoi(literal.c_str());
     std::cout << std::endl;
     std::cout << "float: ";
     std::cout << std::endl;
-    std::cout << "double: ";
+    std::cout << "double: " << number;
     std::cout << std::endl;
 }

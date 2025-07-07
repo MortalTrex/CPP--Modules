19a35e29f847c1ff19f2a47c9fd3065044aa4217
Ralph <balazsra@gmail.com>
2025-07-07 13:04:20 +0200

    start cpp9

---
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,11 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+class BitcoinExchange {
+    private:
+
+};
+
+#endif
---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,15 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Error: wrong number of arguments" << std::endl;
+        return 1;
+    }
+
+    BitcoinExchange btc(argv[1]);
+
+    btc.execute();
+    return 0;
+}
